0d46f9f0a429dd00bb3ba7877c62ec26b223502e
idomaori98 <idoma@mta.ac.il>
2026-01-22 15:38:40 +0200

    hadnling errors with use effect

--- src/App-v1.js
+++ src/App-v1.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 const tempMovieData = [
     {
@@ -45,42 +45,9 @@ const tempWatchedData = [
 const average = (arr) =>
     arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
-const KEY = "40f108b2";
-
 export default function App() {
-    const [movies, setMovies] = useState([]);
-    const [watched, setWatched] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState("");
-    const query = "dfdf";
-
-    useEffect(function () {
-        async function fetchMovies() {
-            try {
-                setIsLoading(true);
-                const res = await fetch(
-                    `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
-                );
-
-                if (!res.ok)
-                    throw new Error(
-                        "Something went wrong with fetching movies"
-                    );
-
-                const data = await res.json();
-                if (data.Response === "False")
-                    throw new Error("Movie not found");
-
-                setMovies(data.Search);
-            } catch (err) {
-                console.error(err.message);
-                setError(err.message);
-            } finally {
-                setIsLoading(false);
-            }
-        }
-        fetchMovies();
-    }, []);
+    const [movies, setMovies] = useState(tempMovieData);
+    const [watched, setWatched] = useState(tempWatchedData);
 
     return (
         <>
@@ -90,10 +57,7 @@ export default function App() {
             </NavBar>
             <Main>
                 <Box>
-                    {/* {isLoading ? <Loader /> : <MovieList movies={movies} />} */}
-                    {isLoading && <Loader />}
-                    {!isLoading && !error && <MovieList movies={movies} />}
-                    {error && <ErrorMessage message={error} />}
+                    <MovieList movies={movies} />
                 </Box>
                 <Box>
                     <WatchedSummary watched={watched} />
@@ -101,18 +65,6 @@ export default function App() {
                 </Box>
             </Main>
         </>
-    );
-}
-
-function Loader() {
-    return <p className="loader">Loading...</p>;
-}
-
-function ErrorMessage({ message }) {
-    return (
-        <p className="error">
-            <span>⛔️</span> {message}
-        </p>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 import StarRating from "./StarRating";
 
 function Test() {
@@ -22,12 +22,12 @@ function Test() {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        {/* <App /> */}
-        <StarRating
+        <App />
+        {/* <StarRating
             maxRating={5}
             messages={["Terrbile", "Bad", "Ok", "Good", "Amazing"]}
-        />
-        <StarRating size={24} color="red" className="text" defaultRating={3} />
-        <Test />
+        /> */}
+        {/* <StarRating size={24} color="red" className="text" defaultRating={3} />
+        <Test /> */}
     </React.StrictMode>
 );
